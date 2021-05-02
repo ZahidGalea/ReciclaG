ff66796e07e97c00c47c5c441a892156d7cdaf40
zahid <zahidale.zg@gmail.com>
2021-05-02 16:07:03 -0400

    It integrates google maps API and modifies script section

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,38 @@
-import { CheckTriggerHorarioBox } from './modules/horarios.js';
+function CheckTriggerHorarioBox() {
+
+    if ($(this).attr("value") == "1") {
+        $(".HorarioBox").hide('slow');
+    }
+    if ($(this).attr("value") == "2") {
+        $(".HorarioBox").show('slow');
+
+    }
+};
+
+function ValidarHorarioBox() {
+    // Nothing here
+}
+
+function mapAddress(mapElement, address) {
+    var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var mapOptions = {
+                zoom: 16,
+                center: results[0].geometry.location,
+                disableDefaultUI: true
+            };
+            var map = new google.maps.Map(mapElement, mapOptions);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
 
 
 $(document).ready(function () {
@@ -7,10 +41,27 @@ $(document).ready(function () {
     * Renderization Part
     */
 
+
     // HORARIO
-    $("#opcionHorario1").trigger('click');
-    // CHECK IF HORARIO MUST BE TRIGGERED
-    $(".opcionHorario").click(CheckTriggerHorarioBox);
+    // Check if horario exists and load script
+    if ($('.horario')[0]) {
+        $("#opcionHorario1").trigger('click');
+        // CHECK IF HORARIO MUST BE TRIGGERED
+        $(".opcionHorario").click(CheckTriggerHorarioBox);
+    }
 
 
+
+
+    // GOOGLE MAPS ON INICIATIVAS PAGE
+    $puntoContent = document.querySelectorAll(".punto_content")
+    $puntoContent.forEach(function (currentValue, currentIndex, listObj) {
+
+        $puntoInformation = $(currentValue).children('.punto_information');
+        $puntoInformationBody = $puntoInformation.children('.card-body')
+        $cardText = $puntoInformationBody.children('.card-text')
+        $direccionPuntoRerciclag = $cardText.children('.direccionPuntoReciclag').text()
+        console.log($direccionPuntoRerciclag)
+        mapAddress($(currentValue).children('.map_canvas')[0], $direccionPuntoRerciclag.concat(" Santiago de chile"));
+    });
 });
