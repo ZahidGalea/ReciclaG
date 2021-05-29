d7ce3e0a38c76a7bfbb17c741d481f3dcccb9f26
zahid <zahidale.zg@gmail.com>
2021-05-29 01:54:50 -0400

    It uploads first version of django structure, It reformats all links to django parametrized urls and image paths. Also creates a base.html that holds the basic structure of each page, in this case Navbar and footer with Django blocks

--- core/views.py
+++ core/views.py
@@ -1,6 +1,51 @@
 from django.shortcuts import render
 
-# Create your views here.
 
-def index():
-    return
+def home(request):
+    context = {}
+    return render(request, 'app/index.html', context=context)
+
+
+def cambioclave(request):
+    context = {}
+    return render(request, 'app/cambioclave.html', context=context)
+
+
+def crearusuario(request):
+    context = {}
+    return render(request, 'app/crearusuario.html', context=context)
+
+
+def dashboard(request):
+    context = {}
+    return render(request, 'app/dashboard.html', context=context)
+
+
+def iniciativa(request):
+    context = {}
+    return render(request, 'app/iniciativa.html', context=context)
+
+
+def inscribir(request):
+    context = {}
+    return render(request, 'app/inscribir.html', context=context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'app/login.html', context=context)
+
+
+def modificar(request):
+    context = {}
+    return render(request, 'app/modificar.html', context=context)
+
+
+def recupclave(request):
+    context = {}
+    return render(request, 'app/recupclave.html', context=context)
+
+
+def verificafono(request):
+    context = {}
+    return render(request, 'app/verificafono.html', context=context)
